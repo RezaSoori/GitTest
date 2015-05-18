e025c50813f9e2c582b306e0bca69eff9ea15dff
Reza <reza@yahoo.com\>
2015-05-18 22:26:50 +1200

    nadaraad

---
+++ GitTest/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace GitTest
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            var a = DateTime.Now;    
+        }
+        
+    }
+}
